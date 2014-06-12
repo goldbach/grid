1abd4191f90b6b74b62d69e3a4a43fc7691ebcc4
goldbach <jakob.goldbach@gmail.com>
2014-06-12 23:54:17 +0200

    router / router

--- pygrid/broker.py
+++ pygrid/broker.py
@@ -7,8 +7,8 @@ clients_url = 'tcp://*:7000'
 workers_url = 'tcp://*:7001'
 
 context = zmq.Context()
-client  = context.socket(zmq.XREP)
-worker  = context.socket(zmq.XREQ)
+client  = context.socket(zmq.ROUTER) # XREP
+worker  = context.socket(zmq.ROUTER) # XREQ 
 
 poller = zmq.Poller()
 poller.register(client, zmq.POLLIN)
@@ -19,20 +19,30 @@ worker.bind(workers_url)
 
 # zmq.device(zmq.QUEUE, client, worker)
 
+lru = []
+
 while True:
     
     socks = dict(poller.poll())
     if client in socks and socks[client] == zmq.POLLIN:
-        print "new client",
-        msg = client.recv_multipart()
-        print msg
-        worker.send_multipart(msg)
+        address, empty, msg = client.recv_multipart()
+        print "client req from", address
+        print "lru q is", lru
+        if len(lru) > 0:
+            wrk = lru.pop()
+            print "sending req to worker", wrk
+            worker.send_multipart([wrk, '', msg])
         
     if worker in socks and socks[worker] == zmq.POLLIN:
         print "new worker",
-        msg = worker.recv_multipart()
-        print msg
-        client.send_multipart(msg)
+        address, empty, msg = worker.recv_multipart()
+        lru.append(address)
+        if msg == "give me work":
+            print "initial worker", address
+        else:
+            print "result from worker", address, msg
+
+
 
     
     
--- pygrid/worker.py
+++ pygrid/worker.py
@@ -1,17 +1,23 @@
 import os
 import zmq
 import time
+import sys
 
 broker_url = 'tcp://localhost:7001'
 
+wid = sys.argv[1]
+
 ctx = zmq.Context()
-broker = ctx.socket(zmq.REP)
+broker = ctx.socket(zmq.REQ)
+broker.setsockopt(zmq.IDENTITY, wid)
 broker.connect(broker_url)
 
+       
+broker.send("give me work")
 while True:
     cmd = broker.recv()
-    print "command is", cmd
-    print "doing heavy work"
+    print wid, "doing heavy work"
     time.sleep(2)
-    broker.send("result is 42")
+    broker.send("result is 42 - give me more work")
+    
     
